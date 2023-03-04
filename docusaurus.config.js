b3cf775a9c016ebf114177d4bc811849e0b05357
DDlia <ponta88@outlook.jp>
2023-03-04 17:05:25 +0900

    fix link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/TechnoTUT/docs.technotut.net_v2/tree/main/',
+            'https://github.com/TechnoTUT/docs.technotut.net/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
